668a4303cb342462bb2d607101a5bc56b4b8291c
KwonNamSon <kwon37xi@gmail.com>
2020-09-03 10:14:06 +0900

    indent to space

--- install_kubuntu.sh
+++ install_kubuntu.sh
@@ -16,52 +16,52 @@ echo "vm.swappiness=5" | sudo tee /etc/sysctl.d/99-vm_swappiness.conf
 echo "### 사용자 디렉토리 변경 ###"
 xdg-user-dirs-update --set DESKTOP $HOME/Desktop
 if [ -d ~/바탕화면 ]; then
-	mv ~/바탕화면 ~/Desktop
+    mv ~/바탕화면 ~/Desktop
 fi
 
 xdg-user-dirs-update --set DOWNLOAD $HOME/Downloads
 if [ -d ~/다운로드 ]; then
-	mv ~/다운로드 ~/Downloads
+    mv ~/다운로드 ~/Downloads
 fi
 
 xdg-user-dirs-update --set TEMPLATES $HOME/Templates
 if [ -d ~/템플릿 ]; then
-	mv ~/템플릿 ~/Templates
+    mv ~/템플릿 ~/Templates
 fi
 
 xdg-user-dirs-update --set PUBLICSHARE $HOME/Public
 if [ -d ~/공개 ]; then
-	mv ~/공개 ~/Public
+    mv ~/공개 ~/Public
 fi
 
 xdg-user-dirs-update --set DOCUMENTS $HOME/Documents
 if [ -d ~/문서 ]; then
-	mv ~/문서 ~/Documents
+    mv ~/문서 ~/Documents
 fi
 
 xdg-user-dirs-update --set MUSIC $HOME/Music
 if [ -d ~/음악 ]; then
-	mv ~/음악 ~/Music
+    mv ~/음악 ~/Music
 fi
 
 xdg-user-dirs-update --set PICTURES $HOME/Pictures
 if [ -d ~/사진 ]; then
-	mv ~/사진 ~/Pictures
+    mv ~/사진 ~/Pictures
 fi
 
 xdg-user-dirs-update --set VIDEOS $HOME/Videos
 if [ -d ~/비디오 ]; then
-	mv ~/비디오 ~/Videos
+    mv ~/비디오 ~/Videos
 fi
 
 echo "### Install base packages ###"
 sudo apt-get install -y apt-transport-https \
-	ca-certificates \
-	curl \
-	gnupg-agent \
-	software-properties-common \
-	software-properties-qt \
-	unzip unrar p7zip-full
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+    software-properties-qt \
+    unzip unrar p7zip-full
 
 
 echo "### 저장소 추가 ###"
@@ -122,58 +122,58 @@ sudo apt-get -y upgrade
 echo "### 필수 소프트웨어 자동 설치"
 # plasma-discover-backend-fwupd 는 설치 불필요
 sudo apt-get install -y inxi \
-	kubuntu-restricted-addons kubuntu-restricted-extras \
-	git \
-	muon aptitude qapt-deb-installer debocker \
-	plasma-discover-backend-flatpak plasma-discover-backend-snap plasma-discover-backend-fwupd \
-	yakuake \
-	dolphin-plugins dolphin-owncloud kio-gdrive dolphin-plugins \
-	plasma-browser-integration \
-	flatpak \
-	vim-gtk \
-	copyq copyq-plugins \
-	grub-customizer \
-	vivaldi-stable \
-	libreoffice libreoffice-help-ko libreoffice-l10n-ko \
-	meld \
-	tlp tlp-rdw tlpui \
-	pinta krita krita-l10n \
-	gimp gimp-gmic \
-	vlc smplayer youtube-dl audacious \
-	ufw gufw \
-	docker docker-compose \
-	ctop \
-	openconnect network-manager-openconnect \
-	openfortivpn network-manager-fortisslvpn \
-	universal-ctags fzf \
-	ripgrep \
-	fonts-alee \
-	fonts-lexi-gulim \
-	fonts-lexi-saebom \
-	fonts-nanum \
-	fonts-nanum-coding \
-	fonts-nanum-eco \
-	fonts-nanum-extra \
-	fonts-naver-d2coding \
-	fonts-unfonts-core \
-	fonts-unfonts-extra \
-	fonts-noto* \
-	uim uim-byeoru \
-	build-essential \
-	code \
-	java-common  \
-	openjdk-11-jdk \
-	adoptopenjdk-11-hotspot adoptopenjdk-8-hotspot \
-	java-11-amazon-corretto-jdk \
-	nodejs \
-	virtualbox-${VIRTUALBOX_VERSION} \
-	freerdp2-x11 \
-	direnv \
-	autojump \
-	unetbootin \
-	asbru-cm  \
-	openssh-server openssh-sftp-server \
-	stow
+    kubuntu-restricted-addons kubuntu-restricted-extras \
+    git \
+    muon aptitude qapt-deb-installer debocker \
+    plasma-discover-backend-flatpak plasma-discover-backend-snap plasma-discover-backend-fwupd \
+    yakuake \
+    dolphin-plugins dolphin-owncloud kio-gdrive dolphin-plugins \
+    plasma-browser-integration \
+    flatpak \
+    vim-gtk \
+    copyq copyq-plugins \
+    grub-customizer \
+    vivaldi-stable \
+    libreoffice libreoffice-help-ko libreoffice-l10n-ko \
+    meld \
+    tlp tlp-rdw tlpui \
+    pinta krita krita-l10n \
+    gimp gimp-gmic \
+    vlc smplayer youtube-dl audacious \
+    ufw gufw \
+    docker docker-compose \
+    ctop \
+    openconnect network-manager-openconnect \
+    openfortivpn network-manager-fortisslvpn \
+    universal-ctags fzf \
+    ripgrep \
+    fonts-alee \
+    fonts-lexi-gulim \
+    fonts-lexi-saebom \
+    fonts-nanum \
+    fonts-nanum-coding \
+    fonts-nanum-eco \
+    fonts-nanum-extra \
+    fonts-naver-d2coding \
+    fonts-unfonts-core \
+    fonts-unfonts-extra \
+    fonts-noto* \
+    uim uim-byeoru \
+    build-essential \
+    code \
+    java-common  \
+    openjdk-11-jdk \
+    adoptopenjdk-11-hotspot adoptopenjdk-8-hotspot \
+    java-11-amazon-corretto-jdk \
+    nodejs \
+    virtualbox-${VIRTUALBOX_VERSION} \
+    freerdp2-x11 \
+    direnv \
+    autojump \
+    unetbootin \
+    asbru-cm  \
+    openssh-server openssh-sftp-server \
+    stow
 
 # flatpak 설치
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -184,30 +184,30 @@ sudo apt-get install --install-recommends winehq-stable winetricks
 
 echo "### install google chrome ###"
 if ! [ -f "/usr/bin/google-chrome" ]; then
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
 fi
 
 echo "### install naver whale ###"
 if ! [ -f "/usr/bin/naver-whale" ]; then
-	wget http://update.whale.naver.net/downloads/installers/naver-whale-stable_amd64.deb  -O ~/Downloads/naver-whale-stable_amd64.deb
-	sudo dpkg -i ~/Downloads/naver-whale-stable_amd64.deb
+    wget http://update.whale.naver.net/downloads/installers/naver-whale-stable_amd64.deb  -O ~/Downloads/naver-whale-stable_amd64.deb
+    sudo dpkg -i ~/Downloads/naver-whale-stable_amd64.deb
 fi
 
 
 echo "### install vagrant ###"
 if ! [ -f "/usr/bin/vagrant" ]; then
-	wget "$VAGRANT_PACKAGE_DOWNLOAD_URL" -O "/tmp/${VAGRANT_DEB_FILENAME}"
-	sudo dpkg -i "/tmp/${VAGRANT_DEB_FILENAME}"
+    wget "$VAGRANT_PACKAGE_DOWNLOAD_URL" -O "/tmp/${VAGRANT_DEB_FILENAME}"
+    sudo dpkg -i "/tmp/${VAGRANT_DEB_FILENAME}"
 fi
 
 echo "### install packer ###"
 if ! [ -f "/opt/packer/packer" ]; then
-	wget "${PACKER_ZIP_DOWNLOAD_URL}" -O /tmp/packer_linux_amd64.zip
-	sudo mkdir -p /opt/packer
-	sudo unzip /tmp/packer_linux_amd64.zip -d /opt/packer
-	sudo chmod 0755 /opt/packer/packer
-	sudo ln -s /opt/packer/packer /usr/local/bin/packer
+    wget "${PACKER_ZIP_DOWNLOAD_URL}" -O /tmp/packer_linux_amd64.zip
+    sudo mkdir -p /opt/packer
+    sudo unzip /tmp/packer_linux_amd64.zip -d /opt/packer
+    sudo chmod 0755 /opt/packer/packer
+    sudo ln -s /opt/packer/packer /usr/local/bin/packer
 fi
 
 echo "### install hugo ###"
@@ -236,28 +236,28 @@ sudo service docker restart
 
 echo "### install custom fonts ###"
 if ! [ -d ~/.fonts/free-korean-fonts ]; then
-	mkdir -p ~/.fonts
-	cd ~/.fonts
-	git clone https://github.com/kwon37xi/free-korean-fonts.git
-	fc-cache -v
+    mkdir -p ~/.fonts
+    cd ~/.fonts
+    git clone https://github.com/kwon37xi/free-korean-fonts.git
+    fc-cache -v
 fi
 
 if ! [ -f "/usr/bin/zoom" ]; then
-	sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxcb-xtest
-	wget https://zoom.us/client/latest/zoom_amd64.deb -O /tmp/zoom_amd64.deb
-	sudo dpkg -i /tmp/zoom_amd64.deb
+    sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxcb-xtest
+    wget https://zoom.us/client/latest/zoom_amd64.deb -O /tmp/zoom_amd64.deb
+    sudo dpkg -i /tmp/zoom_amd64.deb
 fi
 
 echo "### install slack ###"
 if ! [ -f "/usr/bin/slack" ]; then
-	wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-${SLACK_VERSION}-amd64.deb" -O /tmp/slack-desktop-${SLACK_VERSION}-amd64.deb
-	sudo dpkg -i /tmp/slack-desktop-${SLACK_VERSION}-amd64.deb
+    wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-${SLACK_VERSION}-amd64.deb" -O /tmp/slack-desktop-${SLACK_VERSION}-amd64.deb
+    sudo dpkg -i /tmp/slack-desktop-${SLACK_VERSION}-amd64.deb
 fi
 
 echo "### install dbeaver ###"
 if ! [ -f "/usr/bin/dbeaver" ]; then
-	wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -O /tmp/dbeaver-ce_latest_amd64.deb
-	sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb
+    wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -O /tmp/dbeaver-ce_latest_amd64.deb
+    sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb
 fi
 
 echo "### install bitwarden ###"
@@ -276,11 +276,11 @@ sudo ln -s /usr/bin/uim-pref-qt5 /usr/bin/uim-pref-qt4
 
 echo "### install JetBrains Toolbox ###"
 if ! [ -f "$HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox" ]; then
-	wget "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}.tar.gz" -O /tmp/jetbrains-toolbox.tar.gz
-	cd /tmp
-	tar xvzf  jetbrains-toolbox.tar.gz
-	cd "jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}"
-	./jetbrains-toolbox 
+    wget "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}.tar.gz" -O /tmp/jetbrains-toolbox.tar.gz
+    cd /tmp
+    tar xvzf  jetbrains-toolbox.tar.gz
+    cd "jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}"
+    ./jetbrains-toolbox 
 fi
 
 sudo apt-get -y autoremove
