095e2f4835c35832003dd11cc32c841652489d23
KwonNamSon <kwon37xi@gmail.com>
2020-09-13 14:20:32 +0900

    ansible 도입중

--- install_start.sh
+++ install_start.sh
@@ -2,13 +2,16 @@
 # error 발생시 즉각 중단.
 set -eux -o pipefail
 shopt -s failglob
-sudo apt-get -y install git
+
+# 최소 필요사항 설치
+sudo apt-get -y install git ansible
 git clone https://kwon37xi@github.com/kwon37xi/dotfiles.git ~/.dotfiles
 
 cd ~/.dotfiles/_installer
 source ~/.dotfiles/_installer/config_envs
 
-source ~/.dotfiles/_installer/prepare_system_env.sh
+ainsible-playbook ~/.dotfiles/_installer/prepare_system_env_playbook.yml
+
 source ~/.dotfiles/_installer/package_repos_install.sh
 source ~/.dotfiles/_installer/prepare_user_dirs.sh
 source ~/.dotfiles/_installer/kubuntu_package_install.sh
