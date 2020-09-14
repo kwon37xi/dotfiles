2ace7899b7af28e5f9e92ba46be745c086fdbd0c
KwonNamSon <kwon37xi@gmail.com>
2020-09-14 23:06:03 +0900

    freefonts depth

--- _installer/free_korean_fonts_install.sh
+++ _installer/free_korean_fonts_install.sh
@@ -2,7 +2,7 @@ echo "### install custom fonts ###"
 if ! [ -d ~/.fonts/free-korean-fonts ]; then
     mkdir -p ~/.fonts
     cd ~/.fonts
-    git clone https://github.com/kwon37xi/free-korean-fonts.git
+    git clone --depth=1 https://github.com/kwon37xi/free-korean-fonts.git
     fc-cache -v
 fi
 
