db7805deffab4096189bbf6c29d7924141e16144
KwonNamSon <kwon37xi@gmail.com>
2020-09-14 22:45:03 +0900

    ensure dir

--- install_start.sh
+++ install_start.sh
@@ -10,15 +10,15 @@ git clone --depth=1 https://kwon37xi@github.com/kwon37xi/dotfiles.git ~/.dotfile
 cd ~/.dotfiles/_installer
 source ~/.dotfiles/_installer/config_envs
 
-ansible-playbook -v --ask-become-pass ~/.dotfiles/_installer/prepare_system_env_playbook.yml
+ansible-playbook -vv --ask-become-pass ~/.dotfiles/_installer/prepare_system_env_playbook.yml
 
-ansible-playbook -v ~/.dotfiles/_installer/package_repos_playbook.yml
+ansible-playbook -vv ~/.dotfiles/_installer/package_repos_playbook.yml
 
 source ~/.dotfiles/_installer/prepare_user_dirs.sh
 source ~/.dotfiles/_installer/kubuntu_package_install.sh
-ansible-playbook -v ~/.dotfiles/_installer/extra_package_install_playbook.yml
+ansible-playbook -vv ~/.dotfiles/_installer/extra_package_install_playbook.yml
 
-ansible-playbook -v ~/.dotfiles/_installer/package_full_upgrade.yml
+ansible-playbook -vv ~/.dotfiles/_installer/package_full_upgrade.yml
 
 source ~/.dotfiles/_installer/free_korean_fonts_install.sh
 source ~/.dotfiles/_installer/local_apps_install.sh
