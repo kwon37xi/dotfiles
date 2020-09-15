2e9c637215284b1a1f77218858ab97080aa872b0
KwonNamSon <kwon37xi@gmail.com>
2020-09-15 21:03:52 +0900

    add copyq

--- _installer/dotfiles_install.sh
+++ _installer/dotfiles_install.sh
@@ -6,4 +6,5 @@ stow -d $DOTFILES_DIR -t ~/ -S local_bin
 stow -d $DOTFILES_DIR -t ~/ -S htop
 stow -d $DOTFILES_DIR -t ~/ -S git
 stow -d $DOTFILES_DIR -t ~/ -S bashrc.d
+stow -d $DOTFILES_DIR -t ~/ -S copyq
 # todo separate personal / work git config
