008e547356b9f4f7fcc519a5ca1853991c520945
KwonNamSon <kwon37xi@gmail.com>
2020-10-30 00:08:09 +0900

    remove copyq, copyq data는 백업으로 처리한다.

---
+++ local_bin/.local/bin/restore-home.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# todo quit kwallet
+rm -rf ~/.local/backup
+rm -rf ~/.local/shareDBeaverData
+rm -rf ~/.config/asbru
+rm -rf ~/.local/share/kwalletd
+rm -rf ~/.config/vivaldi/Default
+rm -rf ~/.config/JetBrains
+rm -rf ~/.local/share/JetBrains
+rm -rf ~/.config/copyq
+
+tar xvzf home-backups.tar.gz -C ~/
+
+sudo cp ~/.local/backup/etc/NetworkManager/system-connections/* /etc/NetworkManager/system-connections/
+sudo chown root:root /etc/NetworkManager/system-connections/
+sudo chmod 0600 /etc/NetworkManager/system-connections/*
+# todo restart kwallet
