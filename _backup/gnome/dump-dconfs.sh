9a44389a2d8fbf8704677e6f172e1c40bddca32a
KwonNamSon <kwon37xi@gmail.com>
2020-11-25 14:03:34 +0900

    dump-dconfs shell script

--- _backup/gnome/dump-dconfs.sh
+++ _backup/gnome/dump-dconfs.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+cd ~/.dotfiles/_backup/gnome
 dconf dump /org/gnome/mutter/keybindings/ > org_gnome_mutter_keybindings.dump
 dconf dump /org/gnome/shell/keybindings/ > org_gnome_shell_keybindings.dump
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > org_gnome_settings-daemon_plugins_media-keys.dump
+dconf dump /org/gnome/desktop/interface/ > org_gnome_desktop_interface.dump
+dconf dump /org/gnome/desktop/peripherals/touchpad/ > org_gnome_desktop_peripherals_touchpad.dump
+dconf dump /org/gnome/desktop/wm/keybindings/ > org_gnome_desktop_wm_keybindings.dump
+dconf dump /org/gnome/shell/extensions/dash-to-panel/ > org_gnome_shell_extensions_dash-to-panel.dump
+dconf dump /org/gnome/shell/extensions/nothing-to-say/ > org_gnome_shell_extensions_nothing-to-say.dump
+dconf dump /apps/guake/ > apps_guake.dump
