974fd77bfe8b19a5f53cf903026cfe1168f764dd
KwonNamSon <kwon37xi@gmail.com>
2021-01-21 22:25:41 +0900

    dbeaver-ce installer

---
+++ local_bin/.local/bin/dbeaver-ce-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget -O /tmp/dbeaver-ce_latest_amd64.deb "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb"
+sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb
+rm /tmp/dbeaver-ce_latest_amd64.deb
