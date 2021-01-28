8b5306a830757c7e4ae4a00aa4e16e3ac94dbc26
KwonNamSon <kwon37xi@gmail.com>
2021-01-28 22:34:48 +0900

    add more exa aliases

--- bash_it_custom/.config/.bash_it_custom/custom.aliases.bash
+++ bash_it_custom/.config/.bash_it_custom/custom.aliases.bash
@@ -2,15 +2,7 @@
 cite 'about-alias'
 about-alias 'kwonnam''s custom aliases'
 
-## Summary for args to less:
-# less(1)
-#   -M (-M or --LONG-PROMPT) Prompt very verbosely
-#   -I (-I or --IGNORE-CASE) Searches with '/' ignore case
-#   -R (-R or --RAW-CONTROL-CHARS) For handling ANSI colors
-#   -F (-F or --quit-if-one-screen) Auto exit if <1 screen
-#   -X (-X or --no-init) Disable termcap init & deinit
 
-alias ag='ag --smart-case --pager="less -MIRFX"'
 alias ydl="/usr/local/bin/youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
 alias kunzip="unzip -O cp949"
 alias fd=fdfind
@@ -30,3 +22,6 @@ alias aptup='sudo apt update && sudo apt upgrade && sudo apt autoremove'
 # exa as ls
 alias ls=exa
 alias ll='exa -lah'
+alias l='exa -lah'
+alias l1='exa -1'
+
