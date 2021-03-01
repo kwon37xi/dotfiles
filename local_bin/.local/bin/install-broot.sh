97ca79e52b0b06b73e4cf3ca1ae7409f1af7adf8
KwonNamSon <kwon37xi@gmail.com>
2021-03-01 17:02:50 +0900

    install-broot

---
+++ local_bin/.local/bin/install-broot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# https://dystroy.org/broot/
+https://dystroy.org/broot/download/x86_64-linux/broot
+
+sudo rm -f /usr/local/bin/broot
+sudo wget -O /usr/local/bin/broot "https://dystroy.org/broot/download/x86_64-linux/broot"
+sudo chmod 755 /usr/local/bin/broot
+
+# need to install bash shell br function
+broot --print-shell-function bash > $HOME/.config/bashrc.d/broot-br.bashrc
+broot --set-install-state installed
