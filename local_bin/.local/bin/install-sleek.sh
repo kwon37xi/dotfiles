10fcc78f2cb48256f9397036be05286984f2f6d0
KwonNamSon <kwon37xi@gmail.com>
2021-04-05 12:36:07 +0900

    add install-sleek.sh

---
+++ local_bin/.local/bin/install-sleek.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# https://github.com/ransome1/sleek
+APPLICATION_NAME=sleek
+LATEST_PACKAGE_URL=$(curl -sL https://api.github.com/repos/ransome1/sleek/releases/latest | jq -r '.assets[].browser_download_url' | grep -e '.amd64.deb')
+PACKAGE_NAME=$(basename "$LATEST_PACKAGE_URL")
+
+rm -f /tmp/$PACKAGE_NAME
+wget -O "/tmp/$PACKAGE_NAME" "$LATEST_PACKAGE_URL"
+
+sudo dpkg -i /tmp/$PACKAGE_NAME
+
+rm -f /tmp/$PACKAGE_NAME
