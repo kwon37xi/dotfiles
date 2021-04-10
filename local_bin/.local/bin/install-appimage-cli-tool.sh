75f18a36c278dc0433628e2fd43bb411b5a13b55
KwonNamSon <kwon37xi@gmail.com>
2021-04-10 15:39:34 +0900

    add appimage-cli-tool

---
+++ local_bin/.local/bin/install-appimage-cli-tool.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# https://github.com/AppImageCrafters/appimage-cli-tool
+APPLICATION_NAME=appimage-cli-tool
+LATEST_PACKAGE_URL=$(curl -sL https://api.github.com/repos/AppImageCrafters/appimage-cli-tool/releases/latest | jq -r '.assets[].browser_download_url' | grep -e '.*_amd64.deb')
+PACKAGE_NAME=$(basename "$LATEST_PACKAGE_URL")
+
+rm -f /tmp/$PACKAGE_NAME
+wget -O "/tmp/$PACKAGE_NAME" "$LATEST_PACKAGE_URL"
+
+sudo dpkg -i /tmp/$PACKAGE_NAME
+
+rm -f /tmp/$PACKAGE_NAME
