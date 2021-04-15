22b01e0906b7b5900f48f3034b07a2fb2a4a9d6d
KwonNamSon <kwon37xi@gmail.com>
2021-04-15 16:05:20 +0900

    go-jira

---
+++ local_bin/.local/bin/install-go-jira.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# https://github.com/go-jira/jira
+APPLICATION_NAME=go-jira
+GITHUB_REPO=go-jira/jira
+PLATFORM_GREP='linux-amd64'
+LATEST_PACKAGE_URL=$(curl -sL "https://api.github.com/repos/$GITHUB_REPO/releases/latest" | jq -r '.assets[].browser_download_url' | grep -e $PLATFORM_GREP)
+PACKAGE_NAME=$(basename "$LATEST_PACKAGE_URL")
+
+rm -f /tmp/$PACKAGE_NAME
+wget -O "/tmp/$PACKAGE_NAME" "$LATEST_PACKAGE_URL"
+
+sudo mv /tmp/$PACKAGE_NAME /usr/local/bin/jira
+sudo chmod 755 /usr/local/bin/jira
+
+rm -f /tmp/$PACKAGE_NAME
