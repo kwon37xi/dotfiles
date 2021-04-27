3e1e8521c96007d04ea45c4fee6a759864a67f4a
KwonNamSon <kwon37xi@gmail.com>
2021-04-27 10:47:04 +0900

    ibus 를 기본 입력기로, nimf 관련코드 제거

--- _backup/gnome/dump-dconfs.sh
+++ _backup/gnome/dump-dconfs.sh
@@ -9,4 +9,3 @@ dconf dump /org/gnome/desktop/wm/keybindings/ > org_gnome_desktop_wm_keybindings
 dconf dump /org/gnome/shell/extensions/dash-to-panel/ > org_gnome_shell_extensions_dash-to-panel.dump
 dconf dump /org/gnome/shell/extensions/nothing-to-say/ > org_gnome_shell_extensions_nothing-to-say.dump
 dconf dump /apps/guake/ > apps_guake.dump
-dconf dump /org/nimf/ > org_nimf.dump
