0606ca3a3e670eb50baea311acaacf69419d7b93
KwonNamSon <kwon37xi@gmail.com>
2021-08-27 13:29:57 +0900

    ddterm config

--- _backup/gnome/dump-dconfs.sh
+++ _backup/gnome/dump-dconfs.sh
@@ -8,4 +8,5 @@ dconf dump /org/gnome/desktop/peripherals/touchpad/ > org_gnome_desktop_peripher
 dconf dump /org/gnome/desktop/wm/keybindings/ > org_gnome_desktop_wm_keybindings.dump
 dconf dump /org/gnome/shell/extensions/dash-to-panel/ > org_gnome_shell_extensions_dash-to-panel.dump
 dconf dump /org/gnome/shell/extensions/nothing-to-say/ > org_gnome_shell_extensions_nothing-to-say.dump
+dconf dump /com/github/amezin/ddterm/ > com_github_amezin_ddterm.dump
 dconf dump /apps/guake/ > apps_guake.dump
