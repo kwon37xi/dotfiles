2c858716255fbb41c84e6df9e5232742592236e7
KwonNamSon <kwon37xi@gmail.com>
2021-10-11 00:10:29 +0900

    remove pacstall

--- local_bin/.local/bin/install-fblog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo rm -f /tmp/fblog.deb
-curl -sL https://api.github.com/repos/brocode/fblog/releases/latest | jq -r '.assets[].browser_download_url' | grep  '.*.deb' | wget -O /tmp/fblog.deb -i -
-
-sudo dpkg -i /tmp/fblog.deb
-
-sudo rm -f /tmp/fblog.deb
