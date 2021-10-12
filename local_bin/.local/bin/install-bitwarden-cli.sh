f4ee48918f9295b336436953fd106b73d74b3949
KwonNamSon <kwon37xi@gmail.com>
2021-10-12 23:43:03 +0900

    install docker-compose cli plugins

--- local_bin/.local/bin/install-bitwarden-cli.sh
+++ local_bin/.local/bin/install-bitwarden-cli.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# fzf latest release download and install
+# bitwarden-cli latest release download and install
 
 sudo rm -f /tmp/bitwarden-cli.zip
 curl -sL https://api.github.com/repos/bitwarden/cli/releases/latest | jq -r '.assets[].browser_download_url' | grep -e 'bw-linux.*' | wget -O /tmp/bitwarden-cli.zip -i -
