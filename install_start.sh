dbeb11f53f3869b923165184b69b4b9c954b6284
KwonNamSon <kwon37xi@gmail.com>
2021-10-18 22:06:40 +0900

    with ansible.cfg log enabled

--- install_start.sh
+++ install_start.sh
@@ -14,6 +14,8 @@ ansible-galaxy collection install community.general
 
 git clone https://kwon37xi@github.com/kwon37xi/dotfiles.git ~/.dotfiles
 
-ansible-playbook -vv ~/.dotfiles/_installer/main_playbook.yml --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
+ANSIBLE_CONFIG=~/.dotfiles/_installer/ansible.cfg \
+    ansible-playbook -vv ~/.dotfiles/_installer/main_playbook.yml \
+    --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
 
 echo "      installation finished. REBOOT! "
