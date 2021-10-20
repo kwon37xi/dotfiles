8fec47a7be92c3f5e554a647691e709e0b628443
KwonNamSon <kwon37xi@gmail.com>
2021-10-20 23:35:41 +0900

    add gnome-terminal dconf

--- _backup/gnome/dump-dconfs.sh
+++ _backup/gnome/dump-dconfs.sh
@@ -6,6 +6,7 @@ dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > org_gnome_settings-d
 dconf dump /org/gnome/desktop/interface/ > org_gnome_desktop_interface.dump
 dconf dump /org/gnome/desktop/peripherals/touchpad/ > org_gnome_desktop_peripherals_touchpad.dump
 dconf dump /org/gnome/desktop/wm/keybindings/ > org_gnome_desktop_wm_keybindings.dump
+dconf dump /org/gnome/terminal/ > org_gnome_terminal.dump
 dconf dump /org/gnome/shell/extensions/dash-to-panel/ > org_gnome_shell_extensions_dash-to-panel.dump
 dconf dump /org/gnome/shell/extensions/nothing-to-say/ > org_gnome_shell_extensions_nothing-to-say.dump
 dconf dump /com/github/amezin/ddterm/ > com_github_amezin_ddterm.dump
