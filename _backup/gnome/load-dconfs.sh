b457b8ba304744856e19de592ff03f2c6c28becc
KwonNamSon <kwon37xi@gmail.com>
2021-10-20 23:35:41 +0900

    add gnome-terminal dconf

--- _backup/gnome/load-dconfs.sh
+++ _backup/gnome/load-dconfs.sh
@@ -4,6 +4,7 @@ dconf load /org/gnome/desktop/interface/ < ~/.dotfiles/_backup/gnome/org_gnome_d
 dconf load /org/gnome/desktop/wm/keybindings/ < ~/.dotfiles/_backup/gnome/org_gnome_desktop_wm_keybindings.dump
 dconf load /org/gnome/mutter/keybindings/ < ~/.dotfiles/_backup/gnome/org_gnome_mutter_keybindings.dump
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/.dotfiles/_backup/gnome/org_gnome_settings-daemon_plugins_media-keys.dump
+dconf load /org/gnome/terminal/ < ~/.dotfiles/_backup/gnome/org_gnome_terminal.dump
 dconf load /org/gnome/shell/extensions/dash-to-panel/ < ~/.dotfiles/_backup/gnome/org_gnome_shell_extensions_dash-to-panel.dump
 dconf load /org/gnome/shell/extensions/nothing-to-say/ < ~/.dotfiles/_backup/gnome/org_gnome_shell_extensions_nothing-to-say.dump
 dconf load /org/gnome/shell/keybindings/ < ~/.dotfiles/_backup/gnome/org_gnome_shell_keybindings.dump
