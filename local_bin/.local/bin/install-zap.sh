b1d69b21fa627ce86928b33585efa44558c6cd32
KwonNamSon <kwon37xi@gmail.com>
2021-11-17 08:47:41 +0900

    zap executable path 명시

--- local_bin/.local/bin/install-zap.sh
+++ local_bin/.local/bin/install-zap.sh
@@ -2,4 +2,4 @@
 mkdir -p ~/.local/bin
 wget https://github.com/srevinsaju/zap/releases/download/continuous/zap-amd64 -O ~/.local/bin/zap
 chmod +x ~/.local/bin/zap
-zap --help
+~/.local/bin/zap --help
