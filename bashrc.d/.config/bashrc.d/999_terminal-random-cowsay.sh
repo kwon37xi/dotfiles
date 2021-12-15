9f602b20e7d27adf7e836a690713ba83e5c8009d
KwonNamSon <kwon37xi@gmail.com>
2021-12-15 11:57:30 +0900

    vscode terminal tldr disable

--- bashrc.d/.config/bashrc.d/999_terminal-random-cowsay.sh
+++ bashrc.d/.config/bashrc.d/999_terminal-random-cowsay.sh
@@ -2,7 +2,13 @@
 
 # 명시적으로 RANDOM_TLDR=0 이면 출력 안함
 random_tldr="${RANDOM_TLDR:-1}"
+
+# vscode 터미널에서도 출력 안함.
+if [ "$TERM_PROGRAM" = "vscode" ]
+then
+    random_tldr=0
+fi
 if [ $random_tldr -eq 1 ]
-    then
-        \tldr --random | cowsay -W 100 -f tux | lolcat
+then
+    \tldr --random | cowsay -W 100 -f tux | lolcat
 fi
