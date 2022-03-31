6b8de5a33518b62f1b65aa6cfdbe90364c5f90d2
KwonNamSon <kwon37xi@gmail.com>
2022-03-31 14:07:00 +0900

    [bash] exclude cowsay on intelij idea terminal

--- bashrc.d/.config/bashrc.d/999_terminal-random-cowsay.sh
+++ bashrc.d/.config/bashrc.d/999_terminal-random-cowsay.sh
@@ -3,11 +3,18 @@
 # 명시적으로 RANDOM_TLDR=0 이면 출력 안함
 random_tldr="${RANDOM_TLDR:-1}"
 
+# jetbrains intellij 계열 터미널에서 출력 안함.
+if [ "$TERMINAL_EMULATOR" = "JetBrains-JediTerm" ]
+then
+    random_tldr=0
+fi
+
 # vscode 터미널에서도 출력 안함.
 if [ "$TERM_PROGRAM" = "vscode" ]
 then
     random_tldr=0
 fi
+
 if [ $random_tldr -eq 1 ]
 then
     ~/.local/bin/tldr --random | cowsay -W 100 -f tux | lolcat
