726192dd51a7f3f0faba2b9fc73a7fd2aee80be7
KwonNamSon <kwon37xi@gmail.com>
2022-04-27 23:53:26 +0900

    [bash] bash keybinding 과 자동완성 대소문자구분제외, tab 즉시 자동완성

--- bashrc.d/.config/bashrc.d/000_term.sh
+++ bashrc.d/.config/bashrc.d/000_term.sh
@@ -19,8 +19,24 @@ bind -m vi-insert '"\C-x\C-e": edit-and-execute-command'
 
 # ~/.inputrc 에 vi 모드가 있으면, shell-expand-line 이 자동 지정이 안된다. 강제 지정필요.
 # https://unix.stackexchange.com/questions/624388/expand-shell-variable-while-editing-command-line
-# meta(alt)+ctrl+l : shell($VAR) expand
+# meta(alt)+ctrl+l : shell($VAR) expand 환경 변수를 실제값으로 바꿔준다.
 bind '"\e\C-l": shell-expand-line'
 
 # meta(alt)-ctrl-a : alias expand
 bind '"\e\C-a": alias-expand-line'
+
+# 각종 키 바인딩 https://www.youtube.com/watch?v=iKzoYUErEM0
+# archwiki readline  https://wiki.archlinux.org/title/readline
+# history : 몇글자 치고 위화살표/아래화살표 누르면 글자가 앞부분이 매칭되는 히스토리만 나옴
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+
+# Ctrl-b : 부모 디렉토리로
+bind '"\C-b": "cd ..\n"'
+# meta(alt)-s : 현재 명령에 sudo 붙여주기 - vi mode. \C-[ 는 ESC 키를 의미함.
+bind '"\es": "\C-[Isudo \C-[$a"'
+# emacs mode : "\C-asudo \C-e"
+
+# https://brettterpstra.com/2015/07/09/shell-tricks-inputrc-binding-fun/
+# Alt-z : cd - 로 직전 디렉토리로 이동하기
+bind '"\ez": "cd -\n"'
