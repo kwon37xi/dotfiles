6707e386460131bca337f9556095f285276de0c1
KwonNamSon <kwon37xi@gmail.com>
2022-04-29 10:03:58 +0900

    [sdkman] fzf 연동으로 설치,선택을 쉽게

--- bashrc.d/.config/bashrc.d/070_sdkman.sh
+++ bashrc.d/.config/bashrc.d/070_sdkman.sh
@@ -2,5 +2,19 @@
 export SDKMAN_DIR="/home/${USER}/.sdkman"
 [[ -s "/home/${USER}/.sdkman/bin/sdkman-init.sh" ]] && source "/home/${USER}/.sdkman/bin/sdkman-init.sh"
 
-# 아래는 더이상 불필요함
-#source <(sdk completion bash)
+# SDKMAN + fzf functions
+# https://www.grailbox.com/2021/04/switch-among-java-versions-fzf-and-sdkman/
+# 현재 shell 용 java version 지정
+ju() {
+   sdk use java "$(sdk list java | grep 'installed\|local only' | grep -v '>>>' | awk '{print $NF}' | fzf)"
+}
+
+# default java 버전 지정
+jd() {
+   sdk default java "$(sdk list java | grep 'installed\|local only' | awk '{print $NF}' | fzf)"
+}
+
+# install jdk
+ji() {
+   sdk install java "$(sdk list java | tail -n +6 | head -n -5 | grep -v 'installed\|local only' | awk '{print $NF}' | fzf)"
+}
