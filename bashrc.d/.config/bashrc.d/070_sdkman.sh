b830eb4f3f039625fce1c22f4d2290def903c02c
KwonNamSon <kwon37xi@gmail.com>
2022-04-30 14:19:50 +0900

    [fzf] sdkman fzf 로 jdk 목록 출력, git 단축키 fzf-git 으로 파일분리

--- bashrc.d/.config/bashrc.d/070_sdkman.sh
+++ bashrc.d/.config/bashrc.d/070_sdkman.sh
@@ -6,15 +6,36 @@ export SDKMAN_DIR="/home/${USER}/.sdkman"
 # https://www.grailbox.com/2021/04/switch-among-java-versions-fzf-and-sdkman/
 # 현재 shell 용 java version 지정
 ju() {
-   sdk use java "$(sdk list java | grep 'installed\|local only' | grep -v '>>>' | awk '{print $NF}' | fzf)"
+    sdk use java "$(sdk list java | grep 'installed\|local only' | grep -v '>>>' | awk '{print $NF}' | fzf)"
 }
 
 # default java 버전 지정
 jd() {
-   sdk default java "$(sdk list java | grep 'installed\|local only' | awk '{print $NF}' | fzf)"
+    sdk default java "$(sdk list java | grep 'installed\|local only' | awk '{print $NF}' | fzf)"
 }
 
 # install jdk
 ji() {
-   sdk install java "$(sdk list java | tail -n +6 | head -n -5 | grep -v 'installed\|local only' | awk '{print $NF}' | fzf)"
+    sdk install java "$(sdk list java | tail -n +6 | head -n -5 | grep -v 'installed\|local only' | awk '{print $NF}' | fzf)"
 }
+
+# installed JDK list
+_sdkman_ji() {
+    sdk list java | grep 'installed\|local only' | grep -v '>>>' | awk '{print $NF}' |
+        fzf --header "Choose installed JDK"
+}
+
+# JDK list except installed
+_sdkman_jl() {
+    sdk list java | tail -n +6 | head -n -5 | grep -v 'installed\|local only' | awk '{print $NF}' |
+        fzf --header "Choose JDK"
+}
+
+# $- =~ i : interactive shell 일 경우
+if [[ $- =~ i ]]; then
+  # tmux 가 아닌경우 redraw-current-line 이 필요할 수있음.
+  bind '"\er": redraw-current-line'
+  # shell-expand-line 은 bind-x*.bashrc에서 \e\C-l 로 지정했음.
+  bind '"\C-j\C-i": "$(_sdkman_ji)\e\C-l\er"'
+  bind '"\C-j\C-l": "$(_sdkman_jl)\e\C-l\er"'
+fi
