5137b00de06726dff0efee6b10466522d40b0dee
KwonNamSon <kwon37xi@gmail.com>
2022-07-08 16:46:14 +0900

    [mat] eclipse memory analyzer install script

---
+++ local_bin/.local/bin/install-mat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# install MAT
+# 최신 버전 참조 : https://www.eclipse.org/mat/downloads.php
+VERSION=1.13.0
+RELEASE_DATE=20220615
+
+echo "${VERSION}.${RELEASE_DATE}"
+
+TARGET_URL="https://mirror.kakao.com/eclipse/mat/${VERSION}/rcp/MemoryAnalyzer-${VERSION}.${RELEASE_DATE}-linux.gtk.x86_64.zip"
+
+echo $TARGET_URL
+
+rm -rf /tmp/eclipse-mat.zip
+wget -O /tmp/eclipse-mat.zip "${TARGET_URL}"
+
+rm -rf ~/.local/opt/mat
+mkdir -p ~/.local/opt
+unzip /tmp/eclipse-mat.zip -d ~/.local/opt
+
+# 메모리 설정 변경
+sed -i.bak -e "s/Xmx1024m/Xmx8g/g" ~/.local/opt/mat/MemoryAnalyzer.ini
