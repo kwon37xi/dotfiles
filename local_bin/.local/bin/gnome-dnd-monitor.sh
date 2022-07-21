c9c1e5c7daeacb381f133a56b6810bd67e3e5f61
KwonNamSon <kwon37xi@gmail.com>
2022-07-21 17:12:00 +0900

    [gnome] Do not disturb monitor log to syslog

--- local_bin/.local/bin/gnome-dnd-monitor.sh
+++ local_bin/.local/bin/gnome-dnd-monitor.sh
@@ -7,7 +7,7 @@ function change_app_notification() {
 
     for app in "${APPS[@]}"
     do
-        echo "$app 'enable' to -> $enable"
+        logger --tag="gnome-dnd-monitor" "$app 'enable' to -> $enable"
         dconf write "/org/gnome/desktop/notifications/application/${app}enable" $enable
         # show-banners has no effect
         # dconf write "/org/gnome/desktop/notifications/application/${app}show-banners" $enable
@@ -22,6 +22,6 @@ change_app_notification $(gsettings get org.gnome.desktop.notifications show-ban
 while read line
 do
     show_banners=$(echo $line | cut -d' ' -f2)
-    echo "current gnome notifications show-banners : $show_banners"
+    logger --tag="gnome-dnd-monitor" "current gnome notifications show-banners : $show_banners"
     change_app_notification $show_banners
 done < <(gsettings monitor org.gnome.desktop.notifications show-banners)
