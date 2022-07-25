6c216e85b567c082bee4f9c93a294556d14d0817
KwonNamSon <kwon37xi@gmail.com>
2022-07-25 16:26:52 +0900

    [presentatio] Presentation Mode toggle 시 방해금지도 설정

--- local_bin/.local/bin/ddterm-presentation-mode-toggle.sh
+++ local_bin/.local/bin/ddterm-presentation-mode-toggle.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-# ddterm 에 대해 presentation mode 를 toggle 한다
+# ddterm 을 기준으로 presentation mode 를 toggle 한다
 current_font=$(dconf read '/com/github/amezin/ddterm/custom-font')
 current_font="${current_font%\'}"
 current_font="${current_font#\'}"
 current_font_size=$(echo $current_font | awk '{print $NF}')
 case $current_font_size in
-    16)
+    16) # to normal mode
+        notify-send -t 1000 "Presentation Mode OFF"
         dconf write '/com/github/amezin/ddterm/custom-font' "'SauceCodePro Nerd Font Mono 12'"
         dconf write '/com/github/amezin/ddterm/window-size' 1.0
+        gsettings set org.gnome.desktop.notifications show-banners true
+        notify-send -t 1000 "Presentation Mode OFF"
+
         ;;
-    *)
+    *) # to presentation mode
+        notify-send -t 1000 "Presentation Mode ON"
         dconf write '/com/github/amezin/ddterm/custom-font' "'SauceCodePro Nerd Font Mono 16'"
         dconf write '/com/github/amezin/ddterm/window-size' 0.6
+        gsettings set org.gnome.desktop.notifications show-banners false
         ;;
 esac
