9dfbf7406c8aa2aad07bfb6c33001cda6e0a0d5e
KwonNamSon <kwon37xi@gmail.com>
2022-08-16 16:52:02 +0900

    [wrappers-easy-executor] add wrappers easy executor installer

---
+++ local_bin/.local/bin/install-wrappers-easy-executor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://github.com/kwon37xi/wrappers-easy-executor
+
+wget -O ~/.local/bin/gw "https://raw.githubusercontent.com/kwon37xi/wrappers-easy-executor/main/gw" && chmod +x ~/.local/bin/gw
+wget -O ~/.local/bin/mw "https://raw.githubusercontent.com/kwon37xi/wrappers-easy-executor/main/mw" && chmod +x ~/.local/bin/mw
+
