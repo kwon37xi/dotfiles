193e36a3348ace15070efe62f4c83939556ff9f2
KwonNamSon <kwon37xi@gmail.com>
2022-12-30 14:52:58 +0900

    [start] sudo prompt 없애기

--- install_start.sh
+++ install_start.sh
@@ -7,8 +7,9 @@ shopt -s failglob
 set +x
 echo -n "Password for sudo: "
 read -s SUDO_PASSWORD
-echo "$SUDO_PASSWORD" | sudo --non-interactive --stdin add-apt-repository -y ppa:ansible/ansible # ansible ubuntu 기본 버전의 버그가 간혹 있어서 PPA로 설치
-echo "$SUDO_PASSWORD" | sudo --non-interactive --stdin apt-get -y install git ansible xz-utils unzip
+echo "설치를 시작합니다."
+echo "$SUDO_PASSWORD" | sudo --stdin --prompt='' add-apt-repository -y ppa:ansible/ansible # ansible ubuntu 기본 버전의 버그가 간혹 있어서 PPA로 설치
+echo "$SUDO_PASSWORD" | sudo --stdin --prompt='' apt-get -y install git ansible xz-utils unzip
 set -x
 
 ansible-galaxy collection install community.general
