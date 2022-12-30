b3f2ce779951bac1bab68c47fc5eb6a39228d8b2
KwonNamSon <kwon37xi@gmail.com>
2022-12-30 12:05:41 +0900

    ansible -vvv show all errors

--- install_start.sh
+++ install_start.sh
@@ -15,6 +15,6 @@ ansible-galaxy collection install community.general
 git clone https://kwon37xi@github.com/kwon37xi/dotfiles.git ~/.dotfiles
 
 export ANSIBLE_LOG_PATH=~/ansible-dotfiles.log
-ansible-playbook -vv ~/.dotfiles/_installer/main_playbook.yml --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
+ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
 
 echo "      installation finished. REBOOT! "
