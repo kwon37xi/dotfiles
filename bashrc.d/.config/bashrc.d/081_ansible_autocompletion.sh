6ced2d663176d543d8ba402dbc4d41ef83744d37
KwonNamSon <kwon37xi@gmail.com>
2023-01-01 23:23:18 +0900

    [ansible] register-python-argcomplete3 도 작동하게 처리

--- bashrc.d/.config/bashrc.d/081_ansible_autocompletion.sh
+++ bashrc.d/.config/bashrc.d/081_ansible_autocompletion.sh
@@ -1,3 +1,10 @@
+# unbuntu 22.04 는 register-python-argcomplete3 이고 그 이후는 register-python-argcomplete 라서
+# 둘 다 작동하도록 처리
+if command -v register-python-argcomplete3 &> /dev/null
+then
+    alias register-python-argcomplete=register-python-argcomplete3
+fi
+
 eval "$(register-python-argcomplete ansible)"
 eval "$(register-python-argcomplete ansible-config)"
 eval "$(register-python-argcomplete ansible-console)"
