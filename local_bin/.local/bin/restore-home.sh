3ed0359f57d018560675fb3d9668b5f94887af57
KwonNamSon <kwon37xi@gmail.com>
2023-01-14 13:29:26 +0900

    [restore-home] 불필요해진 것들 제거

--- local_bin/.local/bin/restore-home.sh
+++ local_bin/.local/bin/restore-home.sh
@@ -2,12 +2,12 @@
 # todo quit kwallet
 rm -rf ~/.local/backup
 rm -rf ~/.local/shareDBeaverData
-rm -rf ~/.config/asbru
-rm -rf ~/.local/share/kwalletd
-rm -rf ~/.config/vivaldi/Default
+#rm -rf ~/.config/asbru
+#rm -rf ~/.local/share/kwalletd
+#rm -rf ~/.config/vivaldi/Default
 rm -rf ~/.config/JetBrains
 rm -rf ~/.local/share/JetBrains
-rm -rf ~/.config/copyq
+#rm -rf ~/.config/copyq
 rm -rf ~/.local/share/keyrings
 
 tar xvzf home-backups.tar.gz -C ~/
