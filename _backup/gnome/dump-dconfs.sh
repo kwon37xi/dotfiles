b6cd3abcaf927320c451d9d70a68670ab54ee873
KwonNamSon <kwon37xi@gmail.com>
2023-04-21 16:01:07 +0900

    [ulauncher] 단축키를 wayland 기준으로 변경

--- _backup/gnome/dump-dconfs.sh
+++ _backup/gnome/dump-dconfs.sh
@@ -2,6 +2,8 @@
 cd ~/.dotfiles/_backup/gnome
 dconf dump /org/gnome/mutter/keybindings/ > org_gnome_mutter_keybindings.dump
 dconf dump /org/gnome/shell/keybindings/ > org_gnome_shell_keybindings.dump
+
+# custom 단축키
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > org_gnome_settings-daemon_plugins_media-keys.dump
 dconf dump /org/gnome/desktop/interface/ > org_gnome_desktop_interface.dump
 dconf dump /org/gnome/desktop/peripherals/touchpad/ > org_gnome_desktop_peripherals_touchpad.dump
