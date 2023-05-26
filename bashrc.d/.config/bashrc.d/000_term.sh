45d30849cf50fa6f89c0a5c49ab531a3471a2f36
KwonNamSon <kwon37xi@gmail.com>
2023-05-26 11:13:24 +0900

    [term] terminal 기본 편집기 vim 으로 변경

--- bashrc.d/.config/bashrc.d/000_term.sh
+++ bashrc.d/.config/bashrc.d/000_term.sh
@@ -6,7 +6,7 @@ export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
 # history 명령에 날짜 출력
 export HISTTIMEFORMAT="%Y-%m-%d %T "
 
-export VISUAL=nano
+export VISUAL=vim
 export EDITOR=$VISUAL
 export SUDO_EDITOR=$VISUAL
 
