e301289c5497d7e4fdac5579863cddbecfe0d419
KwonNamSon <kwon37xi@gmail.com>
2023-06-08 07:51:10 +0900

    [gnome] app-grid 기본값으로 우분투 오렌지로 지정

--- local_bin/.local/bin/set-app-grid-icon.sh
+++ local_bin/.local/bin/set-app-grid-icon.sh
@@ -6,6 +6,11 @@ mkdir -p ~/.icons/Yaru/symbolic/actions
 rm ~/.icons/Yaru/scalable/actions/view-app-grid-symbolic.png
 rm ~/.icons/Yaru/symbolic/actions/view-app-grid-symbolic.png
 
-cp $1 ~/.icons/Yaru/scalable/actions/view-app-grid-symbolic.png
-cp $1 ~/.icons/Yaru/symbolic/actions/view-app-grid-symbolic.png
+TARGET_ICON="$1"
+
+if [ -z "${TARGET_ICON}" ]; then
+    TARGET_ICON=~/.dotfiles/_icons/ubuntu/cof_orange_hex.png
+fi
+cp "${TARGET_ICON}" ~/.icons/Yaru/scalable/actions/view-app-grid-symbolic.png
+cp "${TARGET_ICON}" ~/.icons/Yaru/symbolic/actions/view-app-grid-symbolic.png
 
