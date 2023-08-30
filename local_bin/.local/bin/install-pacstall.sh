ea50d434b0567529c3991eadc890e5bee57502ab
KwonNamSon <kwon37xi@gmail.com>
2023-08-30 20:55:21 +0900

    [pacstall] install-pacstall with deb

--- local_bin/.local/bin/install-pacstall.sh
+++ local_bin/.local/bin/install-pacstall.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-# pacstall 이 더이상 제대로된 deb 패키지를 제공하고 있지 않음.
-sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+# https://github.com/pacstall/pacstall
+APPLICATION_NAME=pacstall
+GITHUB_REPO=pacstall/pacstall
+LATEST_PACKAGE_URL=$(curl -sL "https://api.github.com/repos/$GITHUB_REPO/releases/latest" | jq -r '.assets[].browser_download_url' | grep '.deb')
+PACKAGE_NAME=$(basename "$LATEST_PACKAGE_URL")
+
+rm -f /tmp/$PACKAGE_NAME
+wget -O "/tmp/$PACKAGE_NAME" "$LATEST_PACKAGE_URL"
+
+sudo dpkg -i "/tmp/$PACKAGE_NAME"
+rm -f /tmp/$PACKAGE_NAME
