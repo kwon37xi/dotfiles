2a37ad80e7eea0baa772c9b7557a8dc152a485ad
KwonNamSon <kwon37xi@gmail.com>
2024-01-23 09:28:00 +0900

    [pdftk] reversed->backwarded, ffbb

--- local_bin/.local/bin/pdf-merge-backwarded.sh
+++ local_bin/.local/bin/pdf-merge-backwarded.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # 단면 스캔으로 하나는 앞에서 부터 하나는 뒤에서부터 스캔된  PDF 파일 두개를 올바른 순서로 합쳐준다.
 if [ "$#" -ne 3 ]; then
-    echo "Usage: pdf-merge-reversed.sh <앞면스캔.pdf> <뒷면스캔.pdf> <결과.pdf>"
+    echo "Usage: pdf-merge-backwarded.sh <앞면스캔.pdf> <뒷면스캔.pdf> <결과.pdf>"
 fi
 
 pdftk A="$1" B="$2" shuffle A Bend-1 output "$3"
