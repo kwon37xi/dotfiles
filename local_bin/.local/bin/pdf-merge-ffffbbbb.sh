16408ad43489e8e624bc91af78073fce7c9ee26f
KwonNamSon <kwon37xi@gmail.com>
2024-01-26 15:05:26 +0900

    [pdf] PDF 병합, 최적화 관련 스크립트 추가

---
+++ local_bin/.local/bin/pdf-merge-ffffbbbb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# 단면 스캔으로 두번에 나눠서
+# 앞면_첫번째, 앞면_두번째, 뒷면_두번째, 뒷면_첫번째 순서인
+# 경우에 대해서 일괄 합치기
+
+if [ "$#" -ne 9 ]; then
+    echo "Usage: pdf-merge-fffbbb.sh <앞면4개> <뒷면4개> <결과.pdf>"
+fi
+
+pdftk "$1" "$2" "$3" "$4" cat output "__f.pdf"
+pdftk "$5" "$6" "$7" "$8" cat output "__b.pdf"
+
+pdftk A="__f.pdf" B="__b.pdf" shuffle A Bend-1 output "$9"
---
+++ local_bin/.local/bin/pdf-opt.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# pdf-opt.sh 원본.pdf
+# 결과는 원본.opt.pdf
+# alpine 버전은 버그가 많았음.
+FILE_NAME_ONLY=`basename "$1" .pdf`
+OPTIMIZED_NAME="${FILE_NAME_ONLY}.opt.pdf"
+
+docker run --rm -i --user "$(id -u):$(id -g)" --workdir /data -v "$PWD:/data" jbarlow83/ocrmypdf --tesseract-timeout=0 --optimize 3 --skip-text "/data/$1" "/data/$OPTIMIZED_NAME"
