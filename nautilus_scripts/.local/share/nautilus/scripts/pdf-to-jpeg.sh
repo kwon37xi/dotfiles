5198f8ac1bb84d942807416db0bf33de1b48279f
KwonNamSon <kwon37xi@gmail.com>
2024-05-02 12:17:15 +0900

    nautilus script pdf-to-jpeg

--- nautilus_scripts/.local/share/nautilus/scripts/pdf-to-jpeg.sh
+++ nautilus_scripts/.local/share/nautilus/scripts/pdf-to-jpeg.sh
@@ -6,6 +6,6 @@ for file in $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS; do
     target_path=$(basename "$file")
     cd "$target_path"
     # file_to_png 는 이미지 파일의 prefix
-    pdftoppm -png "$file" "${file}_to_png"
-    notify-send "PDF를 png 파일로 완료 : " "$file"
+    pdftoppm -jpeg "$file" "${file}_to_png"
+    notify-send "PDF를 jpeg 파일로 완료 : " "$file"
 done
