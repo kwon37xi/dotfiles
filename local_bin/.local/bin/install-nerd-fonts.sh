4ad0f483cca5a9dbc3c6b9612cb61767b80efe54
KwonNamSon <kwon37xi@gmail.com>
2024-06-26 09:12:49 +0900

    install-nerd-font VERSION 변수화

--- local_bin/.local/bin/install-nerd-fonts.sh
+++ local_bin/.local/bin/install-nerd-fonts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # https://www.nerdfonts.com/font-downloads
 
+FONT_VERSION="v3.2.1"
+
 # install_nerd_fonts pckage_name url
 function install_nerd_fonts {
     package_name="nerd-fonts-$1"
@@ -26,14 +28,14 @@ function install_nerd_fonts {
     rm -f $download_file
 }
 
-install_nerd_fonts inconsolata "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip"
-install_nerd_fonts isoveka "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip"
-install_nerd_fonts caskaydia "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip"
-install_nerd_fonts codenewroman "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CodeNewRoman.zip" true
-install_nerd_fonts firacode "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip"
-install_nerd_fonts hack "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip"
-install_nerd_fonts jetbrainsmono "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip"
-install_nerd_fonts saucecodepro "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/SourceCodePro.zip"
-install_nerd_fonts terminus "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Terminus.zip"
+install_nerd_fonts inconsolata "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/Inconsolata.zip"
+install_nerd_fonts isoveka "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/Iosevka.zip"
+install_nerd_fonts caskaydia "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/CascadiaCode.zip"
+install_nerd_fonts codenewroman "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/CodeNewRoman.zip" true
+install_nerd_fonts firacode "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/FiraCode.zip"
+install_nerd_fonts hack "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/Hack.zip"
+install_nerd_fonts jetbrainsmono "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/JetBrainsMono.zip"
+install_nerd_fonts saucecodepro "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/SourceCodePro.zip"
+install_nerd_fonts terminus "https://github.com/ryanoasis/nerd-fonts/releases/download/${FONT_VERSION}/Terminus.zip"
 
 fc-cache -vf
