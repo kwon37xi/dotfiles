1e53ebf6cc2b620cdea863a4fac8e8ffde8d28ab
Son KwonNam <kwon37xi@gmail.com>
2024-12-30 16:23:57 +0900

    fzf 환경변수를 .gnomerc -> environment.d 로 이전

--- bashrc.d/.config/bashrc.d/020_fzf.sh
+++ bashrc.d/.config/bashrc.d/020_fzf.sh
@@ -14,17 +14,4 @@ eval "$(fzf --bash)"
 # ------------
 #source "/home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.bash"
 
-# FZF OPTS from https://www.youtube.com/watch?v=tB-AgxzBmH8
-export FZF_DEFAULT_COMMAND='fdfind --follow --hidden --exclude .git --exclude "*.class" --color=always'
-export FZF_DEFAULT_OPTS='--no-height --ansi --color=bg+:#343d46,gutter:-1,pointer:#ff3c3c,info:#0dbc79,hl:#0dbc79,hl+:#23d18b --bind alt-j:down,alt-k:up'
-
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND --type f"
-export FZF_CTRL_T_OPTS="--preview 'batcat --color=always --line-range :50 {}'"
-
-export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
-#export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
-export FZF_ALT_C_OPTS="--preview 'lsd --tree --depth=2 --color=always {} | head -50'"
-
-export FZF_TMUX=1
-# todo TMUX options..
-
+# 모든 환경변수 설정은 ~/.config/environment.d/999-fzf-env.conf 로 넘김
