562761a9878aaa993e69a19e8a4412ccaa105a3b
Son KwonNam <kwon37xi@gmail.com>
2025-04-03 11:34:09 +0900

    android scrcpy-start-app : 화면 모든 받기

---
+++ local_bin/.local/bin/android-scrcpy-start-app.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# scrcpy 를 이용해 특정 앱을 PC 화면에 별도 실행한다.
+
+# 첫 번째 파라미터에 따라 --new-display 옵션 설정
+if [ "$1" == "wide" ]; then
+    DISPLAY_OPTION="--new-display=1920x1080/240"
+elif [ "$1" == "normal" ]; then
+    DISPLAY_OPTION=""
+else
+    echo "Usage: $0 [wide|normal] <app_package_name>"
+    exit 1
+fi
+
+/home/linuxbrew/.linuxbrew/bin/scrcpy $DISPLAY_OPTION --start-app="$2"
--- local_bin/.local/bin/scrcpy-start-app.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# scrcpy 를 이용해 특정 앱을 PC 화면에 별도 실행한다.
-/home/linuxbrew/.linuxbrew/bin/scrcpy --new-display=1920x1080/240 --start-app="$1"
