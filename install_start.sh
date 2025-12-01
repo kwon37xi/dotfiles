c156c182fc6ce4ead08f5402882af9a7fa024c40
KwonNam Son <kwon37xi@gmail.com>
2025-12-01 13:31:50 +0900

    ubuntu 25.10 -> use ask-become-pass

--- install_start.sh
+++ install_start.sh
@@ -5,11 +5,8 @@ shopt -s failglob
 
 # 최소 필요사항 설치
 set +x
-echo -n "Password for sudo: "
-read -s SUDO_PASSWORD
-echo "설치를 시작합니다."
 #echo "$SUDO_PASSWORD" | sudo --stdin --prompt='' add-apt-repository -y ppa:ansible/ansible # ansible ubuntu 기본 버전의 버그가 간혹 있어서 PPA로 설치
-echo "$SUDO_PASSWORD" | sudo --stdin --prompt='' apt-get -y install git ansible xz-utils unzip
+sudo apt-get -y install git ansible xz-utils unzip
 set -x
 
 # --upgrade 옵션으로 최시 번전 컬렉션을 설치하게 해야 한다.
@@ -18,6 +15,6 @@ ansible-galaxy collection install community.general --upgrade
 git clone https://kwon37xi@github.com/kwon37xi/dotfiles.git ~/.dotfiles
 
 export ANSIBLE_LOG_PATH=~/ansible-dotfiles.log
-ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
+ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml --ask-become-pass
 
 echo "      installation finished. REBOOT! "
