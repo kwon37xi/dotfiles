554ca2fb953b71323bd9eed466ab4410c18e9449
Son KwonNam <kwon37xi@gmail.com>
2025-12-16 11:15:49 +0900

    ANSIBLE_BECOME_PASS 환경변수로 전환

--- install_start-at-task.sh
+++ install_start-at-task.sh
@@ -12,11 +12,11 @@ fi
 # 최소 필요사항 설치
 set +x
 echo -n "Password for sudo: "
-read -s SUDO_PASSWORD
+read -s ANSIBLE_BECOME_PASS
 
 if command -v sudo.ws >/dev/null 2>&1; then
     export ANSIBLE_BECOME_EXE='sudo.ws'
 fi
 
 export ANSIBLE_LOG_PATH=~/ansible-dotfiles-step-at.log
-ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml --start-at-task="$1" --step --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
+ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml --start-at-task="$1"
--- install_start.sh
+++ install_start.sh
@@ -6,10 +6,10 @@ shopt -s failglob
 # 최소 필요사항 설치
 set +x
 echo -n "Password for sudo: "
-read -s SUDO_PASSWORD
+read -s ANSIBLE_BECOME_PASS
 echo "설치를 시작합니다."
-#echo "$SUDO_PASSWORD" | sudo --stdin --prompt='' add-apt-repository -y ppa:ansible/ansible # ansible ubuntu 기본 버전의 버그가 간혹 있어서 PPA로 설치
-echo "$SUDO_PASSWORD" | sudo --stdin --prompt='' apt-get -y install git ansible xz-utils unzip
+#echo "$ANSIBLE_BECOME_PASS" | sudo --stdin --prompt='' add-apt-repository -y ppa:ansible/ansible # ansible ubuntu 기본 버전의 버그가 간혹 있어서 PPA로 설치
+echo "$ANSIBLE_BECOME_PASS" | sudo --stdin --prompt='' apt-get -y install git ansible xz-utils unzip
 set -x
 
 # ubuntu 25.10 에서 sudo-rs 가 기본 sudo 명령이 되었는데, ansible이 아직 이와 호환이 안되기 때문에 구버전인 sudo.ws 로 전환
@@ -23,6 +23,6 @@ ansible-galaxy collection install community.general --upgrade
 git clone https://kwon37xi@github.com/kwon37xi/dotfiles.git ~/.dotfiles
 
 export ANSIBLE_LOG_PATH=~/ansible-dotfiles.log
-ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml --extra-vars="ansible_become_pass='{{ lookup('env', 'SUDO_PASSWORD') }}'"
+ansible-playbook -vvv ~/.dotfiles/_installer/main_playbook.yml
 
 echo "      installation finished. REBOOT! "
