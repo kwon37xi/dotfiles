794fcf10ce957a656c5c920b54fdc65499880a02
Son KwonNam <kwon37xi@gmail.com>
2026-02-12 14:57:30 +0900

    android otg big window

--- local_bin/.local/bin/android-otg.sh
+++ local_bin/.local/bin/android-otg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 adb shell ime set "com.egloos.woongyee.sesak/.SesakKeyboardService"
-/home/linuxbrew/.linuxbrew/bin/scrcpy --otg
+/home/linuxbrew/.linuxbrew/bin/scrcpy --otg --window-height=768 --window-width=432
 adb shell ime set "com.samsung.android.honeyboard/.service.HoneyBoardService"
